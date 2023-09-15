aad6785718669f71a80cd3775c22ece1c70302c2
shubham <shubhamkumarhalwa@gmail.com>
2023-09-16 03:31:51 +0530

    update npm

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
-apt-get install -y npm
-# set -x
-# # Determine the current Git branch
-# current_branch=$(git rev-parse --abbrev-ref HEAD)
 
-# # Define the commands to run for each branch
-# if [ "$current_branch" = "dev" ]; then
-#     # Commands to run for the 'dev' branch
+set -x
+# Determine the current Git branch
+current_branch=$(git rev-parse --abbrev-ref HEAD)
 
-#     # echo "Running commands for 'dev' branch..."
-#     # cd /var/www/html/one
-#     # ls
-#     # pwd
-#     # composer update
-#     # php artisan cache:clear
-#     # php artisan route:clear
+# Define the commands to run for each branch
+if [ "$current_branch" = "dev" ]; then
+    # Commands to run for the 'dev' branch
 
-#     # Add your 'dev' branch-specific commands here
-# elif [ "$current_branch" = "prod" ]; then
-#     # Commands to run for the 'prod' branch
+    # echo "Running commands for 'dev' branch..."
+    # cd /var/www/html/one
+    # ls
+    # pwd
+    # composer update
+    # php artisan cache:clear
+    # php artisan route:clear
 
-#     # echo "Running commands for 'prod' branch..."
-#     # cd /var/www/html/second
-#     # ls
-#     # pwd
-#     # composer update
-#     # php artisan cache:clear
-#     # php artisan route:clear
-#     sudo apt-get install -y npm
+    # Add your 'dev' branch-specific commands here
+elif [ "$current_branch" = "prod" ]; then
+    # Commands to run for the 'prod' branch
 
-#     # Add your 'prod' branch-specific commands here
-# else
-#     # Default commands if the branch doesn't match 'dev' or 'prod'
-#     echo "Running default commands..."
-#     # Add your default commands here
-# fi
+    # echo "Running commands for 'prod' branch..."
+    # cd /var/www/html/second
+    # ls
+    # pwd
+    # composer update
+    # php artisan cache:clear
+    # php artisan route:clear
+    sudo apt-get install -y npm
 
-# # Additional common commands that should run for all branches
-# # Add your common commands here
+    # Add your 'prod' branch-specific commands here
+else
+    # Default commands if the branch doesn't match 'dev' or 'prod'
+    echo "Running default commands..."
+    # Add your default commands here
+fi
 
-# # Exit with a success status code
-# exit 0
+# Additional common commands that should run for all branches
+# Add your common commands here
+
+# Exit with a success status code
+exit 0
