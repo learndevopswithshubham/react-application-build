30c93eeaad106ca5e1268f43e6236b1fb337c1f1
shubham <shubhamkumarhalwa@gmail.com>
2023-09-16 03:14:13 +0530

    update application.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-current_branch=$(git rev-parse --abbrev-ref HEAD)
+# current_branch=$(git rev-parse --abbrev-ref HEAD)
 
-if [ "$current_branch" = "dev" ]; then
-    echo "Running commands for 'dev' branch..."
-    cd /var/www/html/one
-    ls
-    pwd
-    composer update
-    php artisan cache:clear
-    php artisan route:clear
+# if [ "$current_branch" = "dev" ]; then
+#     echo "Running commands for 'dev' branch..."
+#     cd /var/www/html/one
+#     ls
+#     pwd
+#     composer update
+#     php artisan cache:clear
+#     php artisan route:clear
 
-elif [ "$current_branch" = "prod" ]; then
-    echo "Running commands for 'prod' branch..."
-    cd /var/www/html/second
-    ls
-    pwd
-    composer update
-    php artisan cache:clear
-    php artisan route:clear
+# elif [ "$current_branch" = "prod" ]; then
+#     echo "Running commands for 'prod' branch..."
+#     cd /var/www/html/second
+#     ls
+#     pwd
+#     composer update
+#     php artisan cache:clear
+#     php artisan route:clear
 
-else
-    echo "Running default commands..."
-fi
+# else
+#     echo "Running default commands..."
+# fi
 
-exit 0
+# exit 0
