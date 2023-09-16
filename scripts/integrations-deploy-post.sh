e35cf5e6136c66a12460c9449a8c4089fe6f7122
shubham <shubhamkumarhalwa@gmail.com>
2023-09-17 03:27:14 +0530

    update

--- scripts/integrations-deploy-post.sh
+++ scripts/integrations-deploy-post.sh
@@ -2,7 +2,7 @@
     
 if [ "$DEPLOYMENT_GROUP_NAME" == "wsr-code-deploy-new-grp" ]
 then
-    cp -R /var/www/html/my-temp-dir /var/www/html/second
+    cp -R /var/www/html/my-temp-dir/. /var/www/html/second
     sudo rm -rf /var/www/html/my-temp-dir/*
     chown -R ubuntu:ubuntu /var/www/html/second
 
