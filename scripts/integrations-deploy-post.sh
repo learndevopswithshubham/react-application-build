2d78f7b7e0e442e626a89f65dfc1c6f3d102134e
shubham <shubhamkumarhalwa@gmail.com>
2023-09-17 03:24:34 +0530

    update

--- scripts/integrations-deploy-post.sh
+++ scripts/integrations-deploy-post.sh
@@ -11,7 +11,7 @@ fi
 
 if [ "$DEPLOYMENT_GROUP_NAME" == "prod-deployment-shu-grp" ]
 then
-    cp -R /var/www/html/my-temp-dir /var/www/html/one
+    cp -R /var/www/html/my-temp-dir/. /var/www/html/one
     sudo rm -rf /var/www/html/my-temp-dir/*
     chown -R ubuntu:ubuntu /var/www/html/one
 
