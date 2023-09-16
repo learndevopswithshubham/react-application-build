cedaf48b09a9ecf1dab613eb415c8e5b9c6b7163
shubham <shubhamkumarhalwa@gmail.com>
2023-09-17 02:59:20 +0530

    update

--- scripts/integrations-deploy-post.sh
+++ scripts/integrations-deploy-post.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
     
-if [ "$DEPLOYMENT_GROUP_NAME" == "prod-deployment-shu-grp" ]
+if [ "$DEPLOYMENT_GROUP_NAME" == "wsr-code-deploy-new-grp" ]
 then
     cp -R /var/www/html/my-temp-dir /var/www/html/second
     chown -R ubuntu:ubuntu /var/www/html/second
@@ -8,7 +8,7 @@ then
     # Insert other commands that need to run...
 fi
 
-if [ "$DEPLOYMENT_GROUP_NAME" == "wsr-code-deploy-new-grp" ]
+if [ "$DEPLOYMENT_GROUP_NAME" == "prod-deployment-shu-grp" ]
 then
     cp -R /var/www/html/my-temp-dir /var/www/html/one
     chown -R ubuntu:ubuntu /var/www/html/one
