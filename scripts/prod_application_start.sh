06b8d2db5b2c7408f5d0ec74abec5d06f556c81b
shubham <shubhamkumarhalwa@gmail.com>
2023-09-16 19:52:29 +0530

    update prod

--- scripts/prod_application_start.sh
+++ scripts/prod_application_start.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-cd ~/ || { echo "Failed to change to home directory"; exit 1; }
-
-mkdir shubham || { echo "Failed to create directory 'shubham'"; exit 1; }
-
-echo "Directory 'shubham' created successfully"
+cd ~/ && mkdir shubham
