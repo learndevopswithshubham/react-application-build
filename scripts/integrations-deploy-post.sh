f3426a69057f6a820c30658e66b7aeb0f16b4050
shubham <shubhamkumarhalwa@gmail.com>
2023-09-17 03:34:37 +0530

    npm

--- scripts/integrations-deploy-post.sh
+++ scripts/integrations-deploy-post.sh
@@ -5,6 +5,12 @@ then
     cp -R /var/www/html/my-temp-dir/. /var/www/html/second
     sudo rm -rf /var/www/html/my-temp-dir/*
     chown -R ubuntu:ubuntu /var/www/html/second
+    cd /var/www/html/second
+    ls
+    pwd
+    composer update
+    php artisan cache:clear
+    php artisan route:clear
 
     # Insert other commands that need to run...
 fi
@@ -14,6 +20,12 @@ then
     cp -R /var/www/html/my-temp-dir/. /var/www/html/one
     sudo rm -rf /var/www/html/my-temp-dir/*
     chown -R ubuntu:ubuntu /var/www/html/one
+    sudo apt install -y npm
+    # ls
+    # pwd
+    # composer update
+    # php artisan cache:clear
+    # php artisan route:clear
 
 
     # Insert other commands that need to run...
