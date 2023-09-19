e2d01b756b3490e205decd4c087f80817ead72c5
shubham <shubhamkumarhalwa@gmail.com>
2023-09-19 10:31:41 +0530

    prod

--- scripts/integrations-deploy-post.sh
+++ scripts/integrations-deploy-post.sh
@@ -3,7 +3,6 @@
 if [ "$DEPLOYMENT_GROUP_NAME" == "wsr-rebuild-dev" ]
 then
     cp -R /var/www/html/my-temp-dir/. /var/www/html/first
-    sleep 300
     sudo rm -rf /var/www/html/my-temp-dir/*
     chown -R ubuntu:ubuntu /var/www/html/first
     curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
@@ -22,7 +21,6 @@ fi
 if [ "$DEPLOYMENT_GROUP_NAME" == "wsr-rebuild-uni" ]
 then
     cp -R /var/www/html/my-temp-dir/. /var/www/html/second
-    sleep 300
     sudo rm -rf /var/www/html/my-temp-dir/*
     chown -R ubuntu:ubuntu /var/www/html/second
     sudo apt install -y npm
@@ -31,7 +29,6 @@ then
     # composer update
     # php artisan cache:clear
     # php artisan route:clear
-
 
 
     # Insert other commands that need to run...
